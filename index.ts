5aa808c2493af8b86666edbecad22a6570276c7f
Edgar Zapeka <edgar.zapeka@gmail.com>
2021-09-11 19:57:11 -0400

    test jsx

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
+// You need to import `h` factory function as Deno Deploy
+// uses it instead of `React.createElement`
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
+
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello world!", {
-    headers: { "content-type": "text/plain" },
+  // renderToString generates html string from JSX components.
+  const response = new Response(renderToString(<App />), {
+    headers: { "content-type": "text/html; charset=utf-8" },
   });
+
   event.respondWith(response);
 });
